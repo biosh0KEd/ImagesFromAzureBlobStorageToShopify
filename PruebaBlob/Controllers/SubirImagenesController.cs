13bf8e7f98a2b7f5910691a24f1f986968c8c433
Efrain Giron <efrainbond@hotmail.com>
2022-05-25 10:48:56 -0500

    Ya se pueden recuperar los archivos.

---
+++ PruebaBlob/Controllers/SubirImagenesController.cs
@@ -0,0 +1,48 @@
+﻿using Azure.Storage.Blobs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaBlob.Controllers
+{
+    public class SubirImagenesController : Controller
+    {
+        // GET: SubirImagenes
+        public ActionResult Index()
+        {
+            if (TempData.ContainsKey("UploadStatus"))
+			{
+                ViewBag.UploadStatus = TempData["UploadStatus"];
+
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFiles(HttpPostedFileBase[] files)
+        {
+
+            //Ensure model state is valid  
+            if (ModelState.IsValid)
+            {   //iterating through multiple file collection   
+                foreach (HttpPostedFileBase file in files)
+                {
+                    //Checking file is available to save.  
+                    if (file != null)
+                    {
+                        var InputFileName = Path.GetFileName(file.FileName);
+                        Console.WriteLine(InputFileName);
+                        TempData["UploadStatus"] = files.Count().ToString() + " files uploaded successfully.";
+
+                    }
+
+                }
+            }
+            return RedirectToAction("Index");
+        }   
+        
+    }
+}
---
+++ PruebaBlob/Models/SubirImagenesModels.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace PruebaBlob.Models
+{
+	public class SubirImagenesModels
+	{
+    }
+
+    public class FileModel
+    {
+        [Required(ErrorMessage = "Please select file.")]
+        [Display(Name = "Browse File")]
+        public HttpPostedFileBase[] files { get; set; }
+
+    }
+
+
+}
